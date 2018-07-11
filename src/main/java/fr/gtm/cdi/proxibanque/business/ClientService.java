e44bd2320c29f09e90c4d56312ab875cab74ed17
Elvynia <elvynia@gmail.com>
2018-07-11 11:22:36 +0200

    Base working on Tomcat.

--- src/main/java/fr/gtm/cdi/proxibanque/business/ClientService.java
+++ src/main/java/fr/gtm/cdi/proxibanque/business/ClientService.java
@@ -1,11 +1,34 @@
 package fr.gtm.cdi.proxibanque.business;
 
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.gtm.cdi.proxibanque.persistence.ClientDao;
+
 @Named
-public class ClientService {
+@SessionScoped
+public class ClientService implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ClientService.class);
 
 	@Inject
-	private ClientService clientService;
+	private ClientDao clientDao;
+
+	public ClientService() {
+
+	}
+
+	@PostConstruct
+	public void init() {
+		LOGGER.info("CLIENT SERVICE BUILT -------------");
+	}
 }
--- src/main/java/fr/gtm/cdi/proxibanque/persistence/ClientDao.java
+++ src/main/java/fr/gtm/cdi/proxibanque/persistence/ClientDao.java
@@ -1,8 +1,23 @@
 package fr.gtm.cdi.proxibanque.persistence;
 
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
-@Named
-public class ClientDao {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Named
+@ApplicationScoped
+public class ClientDao implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ClientDao.class);
+
+	@PostConstruct
+	public void init() {
+		LOGGER.info("CLIENT DAO BUILT -------------");
+	}
 }
