5b04fd2aa857460a00b6b92fe8657e1e6359eb02
Elvynia <elvynia@gmail.com>
2018-07-12 09:48:56 +0200

    Mise en place de la relation 0-1/0-1 entre Client et Address.

--- src/main/java/fr/gtm/proxibanque/domain/Client.java
+++ src/main/java/fr/gtm/proxibanque/domain/Client.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +25,10 @@ public class Client implements HbEntity {
 	private String firstname;
 	private LocalDate birthDate;
 	private String number;
+
+	@OneToOne
+	@JoinColumn(name = "id_address", referencedColumnName = "id")
+	private Address address;
 
 	public Integer getId() {
 		return id;
@@ -62,6 +68,14 @@ public class Client implements HbEntity {
 
 	public void setNumber(String number) {
 		this.number = number;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 
 }
